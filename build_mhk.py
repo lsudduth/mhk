64e9fd79a95d0121c43ae11b450cc9c8360cf2e5
nick <nickrus@cisco.com>
2020-03-27 16:09:30 +0000

    config corrections

--- build_mhk.py
+++ build_mhk.py
@@ -17,24 +17,37 @@ def main():
     Execution begins here.
     """
 
+    # Read the input data from file
     with open("inputs.yml", "r") as handle:
         initial = safe_load(handle)
+
+    # Create outputs/ directory if it doesn't exist
     if not os.path.exists("outputs"):
         os.makedirs("outputs")
 
+    # Iterate over each node in the node_list with a counter
     for i, node in enumerate(initial["node_list"]):
+        # Extract data for each node and update index with offset
         data = process_node(node)
         data["index"] = i + node["index_offset"]
 
+        # Build jinja2 environment
         j2_env = Environment(loader=FileSystemLoader("."), autoescape=True)
         template_files = glob.glob("templates/*.j2")
+
+        # Create per-node output subdirectory
         output_dir = f"outputs/{node['telephony_prefix']}"
         if not os.path.exists(output_dir):
             os.makedirs(output_dir)
 
+        # Generate all templates
         for template_file in template_files:
+
+            # Load templates and render them
             template = j2_env.get_template(template_file)
             config = template.render(data=data)
+
+            # Write new configs to files with same basename as template
             output_file = os.path.basename(template_file).split(".")[0]
             with open(f"{output_dir}/{output_file}.txt", "w") as handle:
                 handle.write(config)
@@ -45,9 +58,13 @@ def process_node(node):
     Build a data dictionary from data in each node definition.
     """
 
+    # Initialize empty data dict and collect the two main
+    # /23 subnets that make up the greater /22
     data = {}
     network = ipaddress.ip_network(node["ipv4_network"])
     first, second = list(network.subnets())
+
+    # Perform subnetting to collect each individual subnet
     subnets = {
         "data": first,
         "biomed": list(second.subnets(prefixlen_diff=3))[0],
@@ -56,25 +73,33 @@ def process_node(node):
         "special": list(second.subnets(prefixlen_diff=5))[10],
         "loopback": list(second.subnets(prefixlen_diff=5))[11],
     }
-    data.update({"subnets": subnets})
-    data.update({"telephony_prefix": node["telephony_prefix"]})
 
+    # Compute individual IP addresses
     addr = {
-        "rtr": {"lb0": subnets["data"][1], "oobm": subnets["mgmt"][1]},
+        "rtr": {"lb0": subnets["loopback"][1]},
         "dsw": {
+            "lb0": subnets["loopback"][2],
             "data_svi10": subnets["data"][-2],
             "biomed_svi20": subnets["biomed"][-2],
             "voice_svi30": subnets["voice"][-2],
             "mgmt_svi40": subnets["mgmt"][-2],
             "special_svi50": subnets["special"][-2],
         },
-        "asw1": {"mgmt_svi40": subnets["mgmt"][2]},
-        "asw2": {"mgmt_svi40": subnets["mgmt"][3]},
-        "asw3": {"mgmt_svi40": subnets["mgmt"][4]},
-        "asw4": {"mgmt_svi40": subnets["mgmt"][5]},
-        "asw5": {"mgmt_svi40": subnets["mgmt"][6]},
+        "asw1": {"mgmt_svi40": subnets["mgmt"][1]},
+        "asw2": {"mgmt_svi40": subnets["mgmt"][2]},
+        "asw3": {"mgmt_svi40": subnets["mgmt"][3]},
+        "asw4": {"mgmt_svi40": subnets["mgmt"][4]},
+        "asw5": {"mgmt_svi40": subnets["mgmt"][5]},
     }
-    data.update({"addr": addr})
+
+    # Update data dict with new data, then return it
+    data.update(
+        {
+            "subnets": subnets,
+            "telephony_prefix": node["telephony_prefix"],
+            "addr": addr,
+        }
+    )
     return data
 
 
